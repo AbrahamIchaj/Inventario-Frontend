329fb164e010025c1a3efd4add0cb66eb6eb2861
AbrahamIchaj <abrahamichaj95@gmail.com>
2025-04-08 21:37:48 -0600

    Install next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import '../styles/bootstrap.css'
+
+export default function Home() {
+  return (
+    <div className="container mt-5">
+   <h1>Hello Inventory</h1>
+    </div>
+
+    
+  );
+}
+
+
